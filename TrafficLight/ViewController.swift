bac5a491ea99749c3daa04f98e80eb68add9c941
Anton Loskutov <mrloskutov@live.ru>
2022-09-17 23:27:38 +0300

    Create label radius

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,14 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var red: UIView!
-    @IBOutlet var yellow: UIView!
-    @IBOutlet var green: UIView!
+    @IBOutlet var redLable: UIView!
+    @IBOutlet var yellowLable: UIView!
+    @IBOutlet var greenLable: UIView!
     @IBOutlet var actionButton: UIButton!
+    
+    private let colorViewTransparency: CGFloat = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        redLable.alpha = colorViewTransparency
+        yellowLable.alpha = colorViewTransparency
+        greenLable.alpha = colorViewTransparency
         actionButton.layer.cornerRadius = 10
+        redLable.layer.cornerRadius = 75
+        yellowLable.layer.cornerRadius = 75
+        greenLable.layer.cornerRadius = 75
 
     }
     @IBAction func actionButtonDidTapped() {
